14ee593d9e29ce91f1c3e93e481c57a4f404365d
Acretock <George.Goncharov@finch-xr.com>
2021-10-14 19:42:57 +0500

    убейте меня нужно просто пройтись и копировать я не хочу это делать

--- Part2/Part2.cpp
+++ Part2/Part2.cpp
@@ -1,48 +1,57 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
 template<typename T>
 class SimpleVector
 {
 public:
-	SimpleVector() = default;
+	SimpleVector() {
+		data = nullptr;
+		end_ = nullptr;
+	}
 	explicit SimpleVector(size_t size) {
 		data = new T[size];
 		end_ = data + size;
+
 	}
 
 	~SimpleVector() {
-		delete[] data;
-		delete end;
+		cout << "no error 1";
+		//delete[] data;
+		delete end_;
+		cout << "no error 2";
 	}
 	T& operator[](size_t index) {
-		return data[index];
+		return *(data + index);
 	}
-	T& operator[](size_t index) const { return data[index]; }
+	T& operator[](size_t index) const { return *(data + index); }
 	T* begin() { return data; }
 	T* end() { return end_; }
 	T* begin() const { return data; }
 	T* end() const { return end_; }
 	size_t Size() const { return size; }
-	size_t Capacity() const { return end() - begin(); }
+	size_t Capacity() const { return end_ - data; }
 	void PushBack(const T& value) {
 		if (Capacity() == 0) {
-			delete[] data;
-			delete end;
-			size++;
-			data = new T[size];
-			end_ = data + size;
-		} else if (Capacity() == Size()) {
-			auto newSize = Capacity() * 2;
-			T* tmp = new T[newSize];
+			unique_ptr<T*> tmp = new T[1u];
 			tmp = data;
 			delete[] data;
+			data = new T[1u];
+			end_ = data + 1u;
+		} else if (Capacity() == Size()) {
+			auto newSize = Capacity() * 2;
+			//T* tmp = data;
+			T* tmp = new T[newSize];
+			//delete[] data;
+			tmp = data;
 			data = tmp;
-			delete[] tmp;
 			end_ = data + newSize;
 		}
 		*(data + size) = value;
+		auto tmp = *(data + size);
+		tmp++;
+		size++;
 	}
 
 private:
@@ -53,6 +62,20 @@ private:
 };
 
 	int main() {
+		SimpleVector<int> vector{};
+
+		vector.PushBack(123);
+		cout << vector.Capacity() << ' ' << vector.Size() << endl;
+		vector.PushBack(333);
+		cout << vector.Capacity() << ' ' << vector.Size() << endl;
+		vector.PushBack(444);
+		cout << vector.Capacity() << ' ' << vector.Size() << endl;
+		vector.PushBack(434);
+		cout << vector.Capacity() << ' ' << vector.Size() << endl;
+		vector.PushBack(44);
+		cout << vector.Capacity() << ' ' << vector.Size() << endl;
+
+		cout << "no error    " <<  vector[0] << ' ' << vector[1] << ' ' << vector[2] << ' ' << vector[3] << ' ' << vector[4];
 
 		return 0;
 	}
